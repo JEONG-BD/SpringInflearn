fd771626a23005ab412df73560623ce994cf1978
dev-alquimista <dev.byeongdu@gmail.com>
2024-07-14 19:55:56 +0900

    bean extend

---
+++ basic/w00/src/test/java/com/example/w00/beanfind/ApplicationContextExtendFindTests.java
@@ -0,0 +1,83 @@
+package com.example.w00.beanfind;
+
+import com.example.w00.discount.DiscountPolicy;
+import com.example.w00.discount.FixDiscountPolicy;
+import com.example.w00.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextExtendFindTests {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+
+    @Test
+    @DisplayName("parent type")
+    void findBeanByParentTypeDuplicate(){
+        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
+        assertThrows(NoUniqueBeanDefinitionException.class, ()
+        -> ac.getBean(DiscountPolicy.class));
+    }
+
+
+    @Test
+    @DisplayName("parent type by bean name")
+    void findBeanByParentTypeByName(){
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("parent type by sub type")
+    void findBeanByParentTypeBySubType(){
+        DiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("Parent Type All")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        for (String bean : beansOfType.keySet()){
+            System.out.println("key " + bean + " value :" + beansOfType.get(bean));
+        }
+        System.out.println("beanOfType : " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+
+    @Test
+    @DisplayName("Object Type All")
+    void findAllBeanByObject(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String bean : beansOfType.keySet()){
+            System.out.println("key " + bean + " value :" + beansOfType.get(bean));
+        }
+        System.out.println("beanOfType : " + beansOfType);
+
+    }
+
+
+    @Configuration
+    static class TestConfig{
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy(){
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy(){
+            return new FixDiscountPolicy();
+        }
+    }
+}
--- basic/w00/src/test/java/com/example/w00/beanfind/ApplicationContextSameBeanFindTests.java
+++ basic/w00/src/test/java/com/example/w00/beanfind/ApplicationContextSameBeanFindTests.java
@@ -47,6 +47,8 @@ public class ApplicationContextSameBeanFindTests {
     }
 
 
+
+
     @Configuration
     static class SameBeanConfig{
 
