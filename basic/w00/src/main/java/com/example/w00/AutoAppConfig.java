8b3883be433c8b786e364080159d497f53837f90
dev-alquimista <dev.byeongdu@gmail.com>
2024-07-15 23:18:30 +0900

    conflict bean

--- basic/w00/src/main/java/com/example/w00/AutoAppConfig.java
+++ basic/w00/src/main/java/com/example/w00/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.w00;
 
+import com.example.w00.member.MemberRepository;
+import com.example.w00.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,11 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Bean(name="memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+
 
 }
