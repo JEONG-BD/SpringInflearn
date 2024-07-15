61a9210a64f62bc89b22d11bf0e1e9e1dea2fe00
dev-alquimista <dev.byeongdu@gmail.com>
2024-07-15 23:11:23 +0900

    @ComponentScan.Filter

---
+++ basic/w00/src/test/java/com/example/w00/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.example.w00.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ basic/w00/src/test/java/com/example/w00/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.example.w00.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
