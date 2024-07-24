fe70fd3a1aa3e0f64e75e1f705e24ef0846d5eea
JEONG-BD <dev.byeongdu@gmail.com>
2024-07-25 08:54:04 +0900

    Member Check Tests

--- jpa01/w01/src/test/java/com/example/w01/service/MemberServiceTests.java
+++ jpa01/w01/src/test/java/com/example/w01/service/MemberServiceTests.java
@@ -2,11 +2,15 @@ package com.example.w01.service;
 
 import com.example.w01.domain.Member;
 import com.example.w01.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.Assert;
+//import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +28,11 @@ public class MemberServiceTests {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em ;
+
     @Test
+    @Rollback(value = false)
     public void memberJoinTest() throws Exception{
         //given
         Member member = new Member();
@@ -33,6 +41,7 @@ public class MemberServiceTests {
         //when 
         Long savedId = memberService.join(member);
 
+        em.flush();
         //then
         Assert.assertEquals(member, memberRepository.findOne(savedId));
     }
@@ -40,9 +49,19 @@ public class MemberServiceTests {
     @Test
     public void memberCheckTest() throws Exception{
         //given
-         
-        //when 
-        
+        Member member1 = new Member();
+        member1.setName("jeong");
+        Member member2 = new Member();
+        member2.setName("jeong");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
         //then
+        Assertions.fail("예외가 발생!!!!");
     }
 }
