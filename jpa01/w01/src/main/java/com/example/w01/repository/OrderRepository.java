fad55dbbddf19e40f97c69d6c7f6c9d6e1ce3ec1
JEONG-BD <dev.byeongdu@gmail.com>
2024-07-27 14:52:16 +0900

    Order Search

--- jpa01/w01/src/main/java/com/example/w01/repository/OrderRepository.java
+++ jpa01/w01/src/main/java/com/example/w01/repository/OrderRepository.java
@@ -2,10 +2,15 @@ package com.example.w01.repository;
 
 import com.example.w01.domain.Order;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,7 +28,74 @@ public class OrderRepository {
         return em.find(Order.class, orderId);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){
-//        em.createQuery("select m from Order m", Order.class);
-//    }
+    public List<Order> findAll1(OrderSearch orderSearch){
+        /*return em.createQuery("select o from Order o join o.member m" +
+                " where o.status =:status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();*/
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if(orderSearch.getOrderStatus() != null){
+            if(isFirstCondition){
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+              jpql += " and";
+            }
+            jpql += "o.status = :status";
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            if(isFirstCondition){
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += "m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if(orderSearch.getOrderStatus() != null){
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        List<Order> orderList = query.getResultList();
+        return orderList;
+        //return em.createQuery(jpql, Order.class).setMaxResults(1000).getResultList();
+    }
+
+
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+        if(orderSearch.getOrderStatus() != null){
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+
+            Predicate name = cb.like(m.get("name"), "%"+ orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
 }
---
+++ jpa01/w01/src/main/java/com/example/w01/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package com.example.w01.repository;
+
+import com.example.w01.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+}
