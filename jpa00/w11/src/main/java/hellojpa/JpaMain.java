a3b3f9cc342b4e59e9c5501a88d93ea4aa65b4ca
JEONG-BD <dev.byeongdu@gmail.com>
2024-08-15 14:56:18 +0900

    Embedded

--- jpa00/w11/src/main/java/hellojpa/JpaMain.java
+++ jpa00/w11/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import hellojpa.domain.Address;
 import hellojpa.domain.Member;
+import org.hibernate.metamodel.model.domain.internal.MapMember;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,13 +17,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setAddress(new Address("seoul", "123-456","Seoul-ro1"));
-            member.setName("new MemberA");
+//            Member member = new Member();
+//            member.setAddress(new Address("seoul", "123-456","Seoul-ro1"));
+//            member.setName("new MemberA");
+            Address address = new Address("seoul", "123-456", "seoul-ro");
+            Member member1 = new Member();
+            Member member2 = new Member();
 
-            em.persist(member);
+            member1.setName("member1");
+            member2.setName("member2");
 
+            Address newAddress = new Address(address.getCity(), address.getAddress(), address.getZipCode());
+            member1.setAddress(address);
+            member2.setAddress(newAddress);
+            System.out.println("=========");
+            System.out.println(address.equals(newAddress));
+            System.out.println("=========");
 
+            em.persist(member1);
+            em.persist(member2);
+
+            member1.getAddress().setCity("Busan");
 
             tx.commit();
         } catch (Exception ex) {
--- jpa00/w11/src/main/java/hellojpa/domain/Address.java
+++ jpa00/w11/src/main/java/hellojpa/domain/Address.java
@@ -1,6 +1,7 @@
 package hellojpa.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -17,5 +18,42 @@ public class Address {
         this.city = city;
         this.zipCode = zipCode;
         this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address1 = (Address) o;
+        return Objects.equals(city, address1.city) && Objects.equals(zipCode, address1.zipCode) && Objects.equals(address, address1.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, zipCode, address);
     }
 }
