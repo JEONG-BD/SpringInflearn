8d0d9459c67a1b16ee08dfddb1b2747074fbba50
JEONG-BD <dev.byeongdu@gmail.com>
2024-08-18 12:52:51 +0900

    Order Repository

--- jpa02/w01/src/main/java/com/example/w01/repository/OrderRepository.java
+++ jpa02/w01/src/main/java/com/example/w01/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.w01.repository;
 
+import com.example.w01.domain.Order;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,17 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void orderCreate(){
-
+    public void orderCreate(Order order){
+        em.persist(order);
     }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> finaAll(){
+//
+//    }
+
+
 }
