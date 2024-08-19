d9d9643c6593ee185929e6aef35edd5372e7e4e0
JEONG-BD <dev.byeongdu@gmail.com>
2024-08-20 07:54:18 +0900

    Order DTO

--- jpa02/w01/src/main/java/com/example/w01/api/OrderSimpleApiController.java
+++ jpa02/w01/src/main/java/com/example/w01/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package com.example.w01.api;
 
+import com.example.w01.domain.Address;
 import com.example.w01.domain.Order;
+import com.example.w01.domain.OrderStatus;
 import com.example.w01.repository.OrderRepository;
 import com.example.w01.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /*
@@ -27,5 +33,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        /*
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return result;
+        */
+        return orderRepository.findAllByString(new OrderSearch()).stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+
+    }
+
+    @Data
+    static class SimpleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+
+        }
+
     }
 }
