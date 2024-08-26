849970a187a4129d6e71f703179ac7329f242a55
JEONG-BD <dev.byeongdu@gmail.com>
2024-08-27 08:31:50 +0900

    Spring Data JPA Start

---
+++ datajpa/w01/src/main/java/com/example/w01/W01Application.java
@@ -0,0 +1,13 @@
+package com.example.w01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W01Application.class, args);
+    }
+
+}
