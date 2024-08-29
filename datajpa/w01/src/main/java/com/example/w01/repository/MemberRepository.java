609e34da54a7dfe4eefb1f5777ea4ef4210dfcba
JEONG-BD <dev.byeongdu@gmail.com>
2024-08-30 08:04:04 +0900

    Member JpaRepository

--- datajpa/w01/src/main/java/com/example/w01/repository/MemberRepository.java
+++ datajpa/w01/src/main/java/com/example/w01/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import com.example.w01.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
--- datajpa/w01/src/test/java/com/example/w01/entity/MemberTests.java
+++ datajpa/w01/src/test/java/com/example/w01/entity/MemberTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -35,6 +37,15 @@ class MemberTests {
         em.persist(memberB);
         em.persist(memberC);
         em.persist(memberD);
+
+        em.flush();
+        em.clear();
+
+        List<Member> memberList = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : memberList) {
+            System.out.println(member);
+        }
 
 //        Member memberA = new Member("memberA");
 //        memberA.setAge(10);
