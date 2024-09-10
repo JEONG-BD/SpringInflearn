5aa4a1650615415d14fe6bd10266d677c560e488
JEONG-BD <dev.byeongdu@gmail.com>
2024-09-11 08:54:02 +0900

    JPQL vs QueryDSL Test

---
+++ querydsl/w01/src/test/java/com/example/w01/entity/QuerydslBasicTests.java
@@ -0,0 +1,77 @@
+package com.example.w01.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTests {
+
+    @Autowired
+    EntityManager em;
+
+    //bJPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+    @BeforeEach
+    public void before(){
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+    }
+
+    @Test
+    public void startJPQL() throws Exception{
+        //given
+        String qlString =
+                "select m from Member m " +
+                "where m.membername = :membername";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("membername", "member1")
+                .getSingleResult();
+
+        //when
+
+        Assertions.assertThat(findMember.getMembername()).isEqualTo("member1");
+        //then
+    }
+
+
+    @Test
+    public void startQuerydsl() throws Exception{
+        //given
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory.select(m)
+                .from(m)
+                .where(m.membername.eq("member1"))
+                .fetchOne();
+        //when
+
+        Assertions.assertThat(findMember.getMembername()).isEqualTo("member1");
+        //then
+    }
+}
