e1f036c8227ebf28c8452ce84eda0b5e8c3d00bb
JEONG-BD <dev.byeongdu@gmail.com>
2024-09-21 16:00:14 +0900

    last

--- querydsl/w01/src/main/java/com/example/w01/repository/MemberRepository.java
+++ querydsl/w01/src/main/java/com/example/w01/repository/MemberRepository.java
@@ -2,10 +2,13 @@ package com.example.w01.repository;
 
 import com.example.w01.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom{
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByMembername(String membername);
+
+
 }
--- querydsl/w01/src/test/java/com/example/w01/repository/MemberRepositoryTests.java
+++ querydsl/w01/src/test/java/com/example/w01/repository/MemberRepositoryTests.java
@@ -3,6 +3,7 @@ package com.example.w01.repository;
 import com.example.w01.dto.MemberSearchCondition;
 import com.example.w01.dto.MemberTeamDto;
 import com.example.w01.entity.Member;
+import com.example.w01.entity.QMember;
 import com.example.w01.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -126,6 +127,40 @@ class MemberRepositoryTests {
 
 
         //when
+
+        //then
+    }
+    
+    @Test
+    public void querydslPredicatedExecutorTest() throws Exception{
+
+
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        QMember member = QMember.member;
+        Iterable<Member> memberResult = memberRepository.findAll(member.age.between(20, 40).and(member.membername.like("member")));
+        //when 
+
+        for (Member row: memberResult) {
+            System.out.println("member ==" + row);
+        }
 
         //then
     }
