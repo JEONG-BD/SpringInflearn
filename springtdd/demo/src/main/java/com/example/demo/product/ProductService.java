a8950bd8734d0b399eaad4daa2c78d9c3edb2299
JBD <dev.byeongdu@gmail.com>
2024-11-25 23:12:17 +0900

    product controller

--- springtdd/demo/src/main/java/com/example/demo/product/ProductService.java
+++ springtdd/demo/src/main/java/com/example/demo/product/ProductService.java
@@ -1,10 +1,18 @@
 package com.example.demo.product;
 
 import com.example.demo.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Component
+
+@RestController
+@RequestMapping("/products")
 class ProductService {
 
     //private final ProductServicesTest productServicesTest;
@@ -15,8 +23,16 @@ class ProductService {
         this.productPart = productPart;
     }
 
-    public void addProduct(AddProductRequest request) {
+    public void addProductOrigin(AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPart.save(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addProductV1(@RequestBody final AddProductRequest request) {
+        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+        productPart.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
